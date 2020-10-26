c666bcbce12e16da44597756f4ee738981f25c99
Ivaylo Dyakov <ipd3995@gmail.com>
2020-10-27 00:04:44 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Setup from "./tutorial/1-useState/setup/1-error-example";
+
+function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
