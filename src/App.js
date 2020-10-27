6cfee011701298a5a179b2711d3c2759f1822673
Ivaylo Dyakov <ipd3995@gmail.com>
2020-10-27 01:59:55 +0100

    Testing webhook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import Setup from "./tutorial/1-useState/setup/5-useState-counter";
-import Final from "./tutorial/1-useState/final/2-useState-basics";
 
 function App() {
   return (
     <div className="container">
       <Setup />
-      {/* <Final /> */}
     </div>
   );
 }
